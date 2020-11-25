ca4f87d454d592a993c4297e17e3a27489f62f9c
Alam <SHAHZDALAM232223@GMAIL.COM>
2020-11-25 14:17:11 +0530

    Initial Commit

---
+++ view2pdf/src/main/java/com/example/view2pdf/View2Pdf.java
@@ -0,0 +1,14 @@
+package com.example.view2pdf;
+
+import android.util.Log;
+
+public class View2Pdf {
+
+    private static  final String TAG = "View2Pdf";
+
+    public static void d(String message){
+        Log.d(TAG, message);
+    }
+
+}
+https://github.com/ASahu16/View2Pdf-Android.git
